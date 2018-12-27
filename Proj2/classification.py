4bb7af992a45acebdf9a8c399b481d44c34fba79
Nuno Miguel <up201406990@fe.up.pt>
2018-12-27 20:57:43 +0000

    trying to put in dictionary

--- Proj2/classification.py
+++ Proj2/classification.py
@@ -2,13 +2,14 @@ import numpy as np
 from os.path import isfile, join
 from os import listdir
 from random import shuffle
+from PIL import Image
 
 CLASSES = ['arrabida', 'camara', 'clerigos', 'musica', 'serralves']
 
 class DataSetGenerator:
-    # Directory should be Dataset/images
-    def __init__(self, directory):
+    def __init__(self, directory): # Directory should be Dataset/images
         self.directory = directory
+        self.dictionary = {}
         self.labels = self.getLabels()
         self.paths = self.getPaths()
 
@@ -25,20 +26,52 @@ class DataSetGenerator:
             img_lists = []
             path = join(self.directory, label)
             for filename in listdir(path):
-                tokens = filename.split('.')
-                if tokens[-1] == 'jpg' or tokens[-1] == 'jpeg' or tokens[-1] == 'JPG':
-                    image_path = join(path, filename)
-                    img_lists.append(image_path)
+                image_path = join(path, filename)
+                img_lists.append(image_path)
+
             paths.append(img_lists)
 
         paths = [item for sublist in paths for item in sublist] # Flatten the paths array
 
-        shuffle(paths)
-        print(len(paths))
         return paths
+
+def openImage(path):
+    im = Image.open(path)
+    im = (np.array(im))
+
+    r = im[:, :, 0].flatten()
+    g = im[:, :, 1].flatten()
+    b = im[:, :, 2].flatten()
+    label = [1]
+
+    out = np.array(list(label) + list(r) + list(g) + list(b), np.uint8)
+
+    return out
+
+def save_obj(obj, name):
+    with open('obj/' + name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def main():
     generator = DataSetGenerator("Dataset/images")
+
+    data = []
+    labels = []
+    dictionary = {}
+
+    for label in generator.labels:
+        labels.append(label)
+
+    for path in generator.paths:
+        output = openImage(path)
+        data.append(output)
+
+    dictionary["data"] = data
+    dictionary["labels"] = labels
+
+    save_obj(dictionary, 'dataset')
+
+    print(generator.labels)
 
 if __name__ == '__main__':
     main()
